cab820c405f1b2b81cf8c138b0698be735a1c712
ldjimenezt <ldjimenezt@utn.edu.ec>
2025-08-05 22:58:27 -0500

    definiendo templates, agregando Home, HomeTemplate y ThemeContext en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { AuthContextProvider } from './context/AuthContext'
-
+import styled, { ThemeProvider } from "styled-components";
+import { AuthContextProvider } from "./context/AuthContext";
+import { MyRoutes } from "./routes/routes";
+import { createContext, useState } from "react";
+import { Light, Dark } from "./styles/themes";
+export const ThemeContext = createContext(null);
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeuse, setTheme] = useState("dark");
+  const theme = themeuse === "light" ? "light" : "dark";
+  const themeStyle = theme === "light" ? Light : Dark;
 
   return (
-   <>
-    <AuthContextProvider>
-      <span>Hola desde context</span>
-    </AuthContextProvider>
-   </>
-  )
-}
+    <>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <ThemeProvider theme={themeStyle}>
+          <AuthContextProvider>
+            <Container>
+              <section className="contentSidebar">
 
-export default App
+              </section>
+              <section className="contentMenuambur">
+
+              </section>
+              <section className="ContentRoutes">
+
+              </section>
+              <MyRoutes />
+            </Container>
+          </AuthContextProvider>
+        </ThemeProvider>
+      </ThemeContext.Provider>
+    </>
+  );
+}
+const Container = styled.main`
+  display: grid;
+  grid-template-columns: 1fr;
+  background-color: ${(props) => props.theme.bgtotal};
+
+`;
+
+export default App;
